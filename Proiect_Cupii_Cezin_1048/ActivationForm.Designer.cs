cea3bb69d1a3401ba733963b084339befb167db7
Cezin <cezu.98@gmail.com>
2020-05-18 16:22:21 +0300

    Activation code

---
+++ Proiect_Cupii_Cezin_1048/ActivationForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Proiect_Cupii_Cezin_1048 {
+    partial class ActivationForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ActivationForm));
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.textLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonAdaugaModel = new System.Windows.Forms.Button();
+            this.textBoxCod = new System.Windows.Forms.TextBox();
+            this.panelEmail = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClose.FlatAppearance.BorderSize = 0;
+            this.buttonClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClose.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClose.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(96)))), ((int)(((byte)(88)))));
+            this.buttonClose.Location = new System.Drawing.Point(464, 11);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(30, 30);
+            this.buttonClose.TabIndex = 53;
+            this.buttonClose.Text = "O";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // textLabel
+            // 
+            this.textLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.textLabel.Location = new System.Drawing.Point(14, 44);
+            this.textLabel.Name = "textLabel";
+            this.textLabel.Size = new System.Drawing.Size(480, 111);
+            this.textLabel.TabIndex = 52;
+            this.textLabel.Text = "Introdu codul primit prin email";
+            this.textLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(202, 11);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(96, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 51;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonAdaugaModel
+            // 
+            this.buttonAdaugaModel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(104)))), ((int)(((byte)(142)))), ((int)(((byte)(38)))));
+            this.buttonAdaugaModel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonAdaugaModel.FlatAppearance.BorderSize = 0;
+            this.buttonAdaugaModel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonAdaugaModel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonAdaugaModel.ForeColor = System.Drawing.SystemColors.Control;
+            this.buttonAdaugaModel.Location = new System.Drawing.Point(202, 209);
+            this.buttonAdaugaModel.Name = "buttonAdaugaModel";
+            this.buttonAdaugaModel.Size = new System.Drawing.Size(99, 49);
+            this.buttonAdaugaModel.TabIndex = 50;
+            this.buttonAdaugaModel.Text = "OK";
+            this.buttonAdaugaModel.UseVisualStyleBackColor = false;
+            this.buttonAdaugaModel.Click += new System.EventHandler(this.buttonAdaugaModel_Click);
+            // 
+            // textBoxCod
+            // 
+            this.textBoxCod.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(47)))));
+            this.textBoxCod.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxCod.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCod.ForeColor = System.Drawing.Color.White;
+            this.textBoxCod.Location = new System.Drawing.Point(210, 141);
+            this.textBoxCod.Name = "textBoxCod";
+            this.textBoxCod.Size = new System.Drawing.Size(153, 25);
+            this.textBoxCod.TabIndex = 54;
+            // 
+            // panelEmail
+            // 
+            this.panelEmail.BackColor = System.Drawing.Color.White;
+            this.panelEmail.Location = new System.Drawing.Point(145, 172);
+            this.panelEmail.Name = "panelEmail";
+            this.panelEmail.Size = new System.Drawing.Size(218, 1);
+            this.panelEmail.TabIndex = 62;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(141, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 23);
+            this.label1.TabIndex = 63;
+            this.label1.Text = "COD:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(47)))));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(195, 184);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(123, 19);
+            this.linkLabel1.TabIndex = 64;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Retrimitere Email";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(47)))));
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // ActivationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(38)))), ((int)(((byte)(47)))));
+            this.ClientSize = new System.Drawing.Size(504, 268);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelEmail);
+            this.Controls.Add(this.textBoxCod);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.textLabel);
+            this.Controls.Add(this.buttonAdaugaModel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ActivationForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Activation";
+            this.Load += new System.EventHandler(this.ActivationForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label textLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonAdaugaModel;
+        private System.Windows.Forms.TextBox textBoxCod;
+        private System.Windows.Forms.Panel panelEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
---
+++ Proiect_Cupii_Cezin_1048/ActivationForm.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proiect_Cupii_Cezin_1048 {
+    public partial class ActivationForm : Form {
+        Thread th;
+        string connString;
+        string activationCode;
+        string userName;
+        string password;
+        string email2;
+        public ActivationForm(string email) {
+            InitializeComponent();
+            connString = "Provider = Microsoft.ACE.OLEDB.12.0; Data Source = Users.accdb ";
+            OleDbConnection conexiune = new OleDbConnection(connString);
+            conexiune.Open();
+            OleDbCommand comanda = new OleDbCommand("SELECT username,password,email,activationCode from users where email='"+email+"'");
+            comanda.Connection = conexiune;
+            OleDbDataReader reader = comanda.ExecuteReader();
+            while (reader.Read()) {
+                userName = reader["username"].ToString();
+                email2 = reader["email"].ToString();
+                password = reader["password"].ToString();
+                activationCode = reader["activationCode"].ToString();
+
+            }
+            reader.Close();
+            conexiune.Close();
+        }
+
+        private void buttonAdaugaModel_Click(object sender, EventArgs e) {
+            MsgBox msg = new MsgBox();
+            OleDbConnection conexiune = new OleDbConnection(connString);
+            if (textBoxCod.Text=="") {
+                msg.textProperty = "Te rugam introdu un cod";
+                msg.Show();
+            }
+            else 
+                if(textBoxCod.Text!=activationCode) {
+                msg.textProperty = "Codul este incorect!";
+                msg.Show();
+                }
+                    else 
+                     try {
+                    
+
+                    this.Close();
+                    th = new Thread(openApp);
+                    th.SetApartmentState(ApartmentState.STA);
+                    th.Start();
+                }
+                catch (Exception ex) {
+                    //MessageBox.Show(ex.Message);
+                        }
+        }
+
+        private void openApp() {
+            Application.Run(new Form1());
+        }
+
+        string MakeImageSrcData(string filename) {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            byte[] filebytes = new byte[fs.Length];
+            fs.Read(filebytes, 0, Convert.ToInt32(fs.Length));
+            return "data:image/png;base64," +
+              Convert.ToBase64String(filebytes, Base64FormattingOptions.None);
+        }
+
+        private void sendEmail() {
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+            mail.From = new MailAddress("onlylogistics2020@gmail.com");
+            mail.To.Add(email2);
+            mail.Subject = "Bine ai venit in lumea Only Logistics";
+            mail.IsBodyHtml = true;
+            //string htmlBody = File.ReadAllText("email.html");
+            string htmlTemplate = "<!DOCTYPE html> <html> <head> </head> <body style=\"margin: 0; padding: 0;\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td style=\"padding: 10px 0 30px 0;\"> <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\" style=\"border: 1px solid #cccccc; border-collapse: collapse;\"> <tr> <td align=\"center\" bgcolor=\"#70bbd9\" style=\"padding: 40px 0 30px 0; background-color: #153643; font-size: 28px; font-weight: bold; font-family: 'Montserrat' , sans-serif;\"> <img src=\"" + MakeImageSrcData("logo2.0.png") + "\" alt=\"Creating Email Magic\" width=\"200\" height=\"200\" style=\"display: block;\" /> </td> </tr> <tr> <td bgcolor=\"#ffffff\" style=\"padding: 40px 30px 40px 30px;\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td style=\"color: #153643; font-family: Arial, sans-serif; font-size: 24px;\"> <b>Bine ai venit, {0} </b> </td> </tr> <tr> <td style=\"padding: 20px 0 20px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;\"> Contul tău a fost creat cu success și din acest moment ești rezident oficial al lumii Only Logistics. <p><b>Datele tale de acces sunt:</b></p> <p>Nume de utilizator: {1}</p> <p>Parola: {2}</p> <p><b>Codul tau de verificare este</b></p> <p><b style=\"text-align: center; font-size: 22px;\"> {3} </b></p> <p>Suntem bucurosi ca ai ales serviciile noastre.</p> <b style=\"text-align: center; font-size: 18px;\">Aici sunt doar cateva avantaje in folosirea aplicatiei noastre:</b> </td> </tr> <tr> <td> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td width=\"260\" valign=\"top\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td> <img src=\"" + MakeImageSrcData("left.png") + "\" alt=\"\" width=\"100%\" height=\"140\" style=\"display: block;\" /> </td> </tr> <tr> <td style=\"padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px; text-align: justify\"> Cu ajutorul Only Logistics poti inregistra cu usurinta toate noile masini, soferi, rute sau transporturi. In cadrul aplicatiei noastre poti adauga si modele fiecarei masini, km condusi de soferi in fiecare luna, greutate fiecarui colet, si fiecare oprire a rutelor </td> </tr> </table> </td> <td style=\"font-size: 0; line-height: 0;\" width=\"20\"> &nbsp; </td> <td width=\"260\" valign=\"top\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td> <img src=\"" + MakeImageSrcData("right.png") + "\" alt=\"\" width=\"100%\" height=\"140\" style=\"display: block;\" /> </td> </tr> <tr> <td style=\"padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px; text-align: justify;\"> Prin aplicatia noastra poti uita de cautarea prin rapoarte, deoarece acestea pot fi primite printr-un singur click, direct pe Email. Raporturile contin toate elementele, cat si un grafic care arata numarul de km,salarii medii. </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> <tr> <td bgcolor=\"#ee4c50\" style=\"padding: 30px 30px 30px 30px;\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td style=\"color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;\" width=\"75%\"> &reg; Cezin Cupii, Bucharest 2020<br /> </td> <td align=\"right\" width=\"25%\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"> <tr> <td style=\"font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;\"> <a href=\"http://www.twitter.com/\" style=\"color: #ffffff;\"> <img src=\"" + MakeImageSrcData("tw.png") + "\" alt=\"Twitter\" width=\"38\" height=\"38\" style=\"display: block;\" border=\"0\" /> </a> </td> <td style=\"font-size: 0; line-height: 0;\" width=\"20\">&nbsp;</td> <td style=\"font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;\"> <a href=\"http://www.twitter.com/\" style=\"color: #ffffff;\"> <img src=\"" + MakeImageSrcData("fb.png") + "\" alt=\"Facebook\" width=\"38\" height=\"38\" style=\"display: block;\" border=\"0\" /> </a> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </body> </html>";
+            string mailBody = string.Format(htmlTemplate, userName, userName, password,activationCode);
+            //mail.Body = "<body style=\"margin: 0; padding: 0;\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td style=\"padding: 10px 0 30px 0;\"> <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"600\" style=\"border: 1px solid #cccccc; border-collapse: collapse;\"> <tr> <td align=\"center\" bgcolor=\"#70bbd9\" style=\"padding: 40px 0 30px 0; color: #153643; font-size: 28px; font-weight: bold; font-family: Arial, sans-serif;\"> </td> </tr> <tr> <td bgcolor=\"#ffffff\" style=\"padding: 40px 30px 40px 30px;\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td style=\"color: #153643; font-family: Arial, sans-serif; font-size: 24px;\"> <b>Lorem ipsum dolor sit amet!</b> </td> </tr> <tr> <td style=\"padding: 20px 0 30px 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;\"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat. </td> </tr> <tr> <td> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td width=\"260\" valign=\"top\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td> </td> </tr> <tr> <td style=\"padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;\"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat. </td> </tr> </table> </td> <td style=\"font-size: 0; line-height: 0;\" width=\"20\"> &nbsp; </td> <td width=\"260\" valign=\"top\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td> </td> </tr> <tr> <td style=\"padding: 25px 0 0 0; color: #153643; font-family: Arial, sans-serif; font-size: 16px; line-height: 20px;\"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat. </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> <tr> <td bgcolor=\"#ee4c50\" style=\"padding: 30px 30px 30px 30px;\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" width=\"100%\"> <tr> <td style=\"color: #ffffff; font-family: Arial, sans-serif; font-size: 14px;\" width=\"75%\"> &reg; Someone, somewhere 2013<br /> <a href=\"#\" style=\"color: #ffffff;\"> <font color=\"#ffffff\">Unsubscribe</font> </a> to this newsletter instantly </td> <td align=\"right\" width=\"25%\"> <table border=\"0\" cellpadding=\"0\" cellspacing=\"0\"> <tr> <td style=\"font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;\"> </a> </td> <td style=\"font-size: 0; line-height: 0;\" width=\"20\">&nbsp;</td> <td style=\"font-family: Arial, sans-serif; font-size: 12px; font-weight: bold;\"> </a> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </td> </tr> </table> </body> ";
+            mail.Body = mailBody;
+
+
+
+            //parolafoarteputernica
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("onlylogistics2020@gmail.com", "parolafoarteputernica");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            sendEmail();
+        }
+
+        private void ActivationForm_Load(object sender, EventArgs e) {
+            sendEmail();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e) {
+            Application.Exit();
+        }
+    }
+}
